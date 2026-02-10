c4d90be82fe44bc8ff121b55aed9509b98dae491
magitrepat01-cpu <magi.trepat01@estudiant.upf.edu>
2026-02-10 11:31:24 +0100

    Actualització funcions game.c

--- student/game.c
+++ student/game.c
@@ -96,22 +96,108 @@ void choose_level(Game *game){
 
 /**** LAB 1 - functions to program (start here) ****/
 void print_state(State s){
-    // ToDo - Lab 1
+    for(int i=0; i<s.rows; i++){ //recorre les files
+        for(int j = 0; j < s.columns; j++){ //recorre les columnes
+            printf("%c", s.grid[i][j]); //imprimeix el caracter que hi ha a la posició i,j del tauler, que pot ser una paret, un espai buit, una caixa, etc.
+        }
+        printf("\n"); //imprimeix un salt de línia després d'imprimir cada fila del tauler, per tal de que el tauler es vegui com una matriu.
+    }
 }
 
 void print_game(Game game){
-    // ToDo - Lab 1
+    printf("Puntuació: %d\n", game.score); //imprimeix la puntuació
+    printf("Nivell: %d\n", game.level); //imprimeix el nivell
+    print_state(game.state);  //imprimeix el tauler
 }
 
 
 bool is_terminal(State s){
-    // ToDo - Lab 1
-    return false;
+    int i, j;    
+    for(i = 0; i < s.rows; i++){ // recorre les files
+        for(j = 0; j < s.columns; j++){ // recorre les columnes
+            if(s.grid[i][j] == 'B') //si hi ha una caixa que no està en una posició de meta, el joc no ha acabat
+                return false;
+        }
+    }
+    return true;  //si no hi ha cap caixa que no estigui en una posició de meta, el joc ha acabat
 }
 
 State move(State s, Option o){
-	// ToDo - Lab 1
-    return s;
+	int dr = 0, dc = 0; //representen el canvi de coordenades de la posició de l'agent segons la direcció del moviment
+    switch(o){ //segons la direcció del movimient, s'asigna un valor a dr i dc per indicar el canvi de les coord 
+        case MOVE_UP:  //cap amunt, la fila de l'agent disminuirà en 1 i la columna no canviarà
+            dr = -1;
+            break;
+        case MOVE_RIGHT://cap a la dreta, la fila de l'agent no canviarà i la columna augmentarà en 1
+            dc = 1;
+            break;
+        case MOVE_DOWN://cap avall, la fila de l'agent augmentarà en 1 y la columna no canviarà
+            dr = 1;
+            break;
+        case MOVE_LEFT://cap a l'esquerra, la fila de l'agent no canviarà y la columna disminuirà en 1
+            dc = -1;
+            break;
+        default:   //si l'opció no és un moviment vàlid, es retorna el mateix estat sense canvis
+            return s;
+    }
+    int r = -1;   //inicialitza les coordenades de la posició de l'agent a -1, -1, que és un valor no vàlid per al tauler
+    int c = -1;  
+    for(int i = 0; i < s.rows; i++){ //recorre les files del tauler per trobar la posició actual de l'agent
+        for(int j = 0; j < s.columns; j++){ //recorre las columnes del tauler per trobar la posición actual de l'agent
+            if(s.grid[i][j] == 'A' || s.grid[i][j] == 'Y'){ //si troba el caràcter 'A' o 'Y', s'asignen les coord a r i c
+                r = i;
+                c = j;
+            }
+        }
+    }
+
+    int nr = r + dr; //calcula les coordenades de la posició a la que es vol moure l'agent
+    int nc = c + dc;
+
+    int nnr = r + 2*dr; //calcula les coordenades de la posició a la que es vol moure l'agent si hi ha una caixa en el camí
+    int nnc = c + 2*dc;
+
+    if( nr < 0 || nr >= s.rows || nc < 0 || nc >= s.columns) //si la posició a la que es vol moure l'agent està fora del tauler, retorna el mateix estat
+        return s;
+
+    char next = s.grid[nr][nc]; //guarda el caràcter que hi ha a la posició a la que es vol moure l'agent
+
+    if(next == '.' || next == 'G'){ //si la posició a la que es vol moure l'agent és un espai buit o una posició de meta, es mou l'agent a aquesta posició
+        if(s.grid[r][c] == 'Y') //si l'agent està en una posició de meta, es deixa un 'G' a la posició anterior, sinó es deixa un '.' a la posició anterior
+            s.grid[r][c] = 'G';
+        else
+            s.grid[r][c] = '.';
+        if(next == 'G')       //si la posició a la que es vol moure l'agent és una posició de meta, es deixa un 'Y' a aquesta posició, sinó es deixa un 'A'
+            s.grid[nr][nc] = 'Y';
+        else
+            s.grid[nr][nc] = 'A';
+
+        return s;
+    }
+    if(next == 'B' || next == 'X'){ //si la posició a la que es vol moure l'agent és una caixa, es comprova si es pot empenyer la caixa a la següent posició
+        if(nnr < 0 || nnr >= s.rows || nnc < 0 || nnc >= s.columns) //si la posició a la que es vol moure la caixa está fora del tauler, retorna el mateix estat
+            return s;
+
+        char next2 = s.grid[nnr][nnc]; //guarda el caràcter que hi ha a la posició a la que es vol moure la caixa
+            if(next2 == 'G') //si la posició a la que es vol moure la caixa és una posició de meta, es mou la caixa a aquesta posició i es mou l'agent a la posició anterior de la caixa
+                s.grid[nnr][nnc] = 'X';
+            else
+                s.grid[nnr][nnc] = 'B';
+            
+
+            if(next == 'X') //si la posició a la que es vol moure el agent és una caixa que ya está en una posició de meta, es deixa un 'G' a la posició anterior de la caixa, sinó se deixa un '.' a la posició anterior de la caixa
+                s.grid[nr][nc] = 'Y';
+            else
+                s.grid[nr][nc] = 'A';
+            
+            if(s.grid[r][c] == 'Y') //si el agent está en una posición de meta, es deixa una 'G' a la posició anterior de l'agent, sinó es deixa un '.' a la posició anterior
+                s.grid[r][c] = 'G';
+            else
+                s.grid[r][c] = '.';
+
+            return s;//si es pot moure la caixa, es mou l'agent a la posició anterior de la caixa i es retorna el nou estat del tauler
+    }
+    return s; //si la posició a la que es vol moure l'agent és una paret o qualsevol altre caràcter que no sigui un espai buit, meta o caixa, retorna el mateix estat sense canvis
 }
 
 /**** LAB 1 - functions to program (end here) ****/
