48b2b3b853a39b9c091e07588877f3090b723ee1
magitrepat01-cpu <magi.trepat01@estudiant.upf.edu>
2026-02-23 16:40:14 +0100

    Funcions session fetes

--- student/session.c
+++ student/session.c
@@ -2,30 +2,31 @@
 
 // LAB 1 - functions 
 void restart_session_game(Session *session){
-    //ACTUALITZAR
-    //frees the current game and initializes it again.
-    if(session != NULL){
-        free_session(session);
+    //OBJECTIU DE LA FUNCIÓ --> Finalitzar la partida i reiniciar-la de nou
+    if(session != NULL){        //OBSERVEM SI LA SESSIÓ ACTUAL JA ESTÀ EN PARTIDA O NO (SI EN CARA NO HA COMENÇAT NO CAL FINALITZAR-LA, SINÓ QUE INICIAR-LA)
+        free_session(&session);  //FINALITZEM LA PARTIDA ACTUAL
     }
-    init_game(&session ->current_game);
+    init_game(&session ->current_game); //INICIEM LA PARTIDA DE NOU
 }
+
 
 void init_session(Session *session){
     //ACTUALITZAR
     for(int level = 0; level < MAX_LEVELS; level++){
         session ->best_score[level] = 0;
     }
-    restart_session_game(session);
+    init_game(&session ->current_game);
 }
 
 void print_session(Session *session){
-     unsigned level = session -> current_game.level;
+    unsigned level = session -> current_game.level;
     printf("Millor puntuació: %d\n", session-> best_score[level]);
     print_game(session -> current_game);
 }
 
+
 void new_game_score(Session *session){
-    unsigned level = session -> current_game.level;
+     unsigned level = session -> current_game.level;
     int score = session -> current_game.score;
     if(session -> best_score[level] == 0 || score < session -> best_score[level]){ 
         session -> best_score[level] = score;
@@ -34,18 +35,12 @@ void new_game_score(Session *session){
 
 // LAB 2 - functions
 void free_session(Session *s){
-	// ToDo - Lab 2
+	
     //OBJECTIU HA DE SER ALLIBERAR LA MEMÒRIA OCUPADA PER AQUESTA PARTIDA
     //s->current_game.state.grid;
 
     if(s != NULL){
         free_game(&(s->current_game));
     }
-
-    // ALLIBERACIÓ MEMÒRIA D'UNA MATRIU - EXEMPLE
-    /*for(int i = 0; i < 2; i++){
-	    free(matrix[i]);
-    }
-    free(matrix);*/
 
 }
